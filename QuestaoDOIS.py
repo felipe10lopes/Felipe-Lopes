0a3e7ff7e87975a291364cd38366252bb3e99a35
Felipe Lopes <catce.2023111mtds0040@aluno.ifpi.edu.br>
2023-03-15 16:52:41 -0300

    Questao 2

---
+++ QuestaoDOIS.py
@@ -0,0 +1,23 @@
+
+def area_perimetro(a):
+    
+    return a * a
+
+def perimetro(a):
+    
+    return a * 4
+
+a = float(input())
+
+x = area_perimetro(a)
+
+y = perimetro(a)
+
+
+
+print(f"{x:10.4f}")
+
+print(f"{y:10.4f}")
+
+
+
