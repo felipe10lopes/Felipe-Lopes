be7a5d11b961690e349e361d2231dfb56f5eba8e
Felipe Lopes <catce.2023111mtds0040@aluno.ifpi.edu.br>
2023-03-15 16:49:30 -0300

    primeiro commit

---
+++ QuestaoUM.py
@@ -0,0 +1,16 @@
+
+def calcular(a, b, c):
+    return 2 * a + 5 * b - c
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+
+def main():
+    x = calcular(a,b,c)
+    print(f"{x}")
+
+if  __name__ == '__main__':
+    main()
+    
